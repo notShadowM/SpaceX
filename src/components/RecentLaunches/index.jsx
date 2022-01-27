362ab50e904410713fab9a802873a49f14b56558
notShadowM <maanabahin2001@gmail.com>
2022-01-27 13:27:01 +0200

    fix for smallData

--- src/components/RecentLaunches/index.jsx
+++ src/components/RecentLaunches/index.jsx
@@ -100,112 +100,6 @@ const columns = [
   },
 ];
 
-const columnsX = [
-  {
-    dataIndex: 'index',
-    valueType: 'indexBorder',
-    width: 48,
-  },
-  {
-    title: '标题',
-    dataIndex: 'title',
-    copyable: true,
-    ellipsis: true,
-    tip: '标题过长会自动收缩',
-    formItemProps: {
-      rules: [
-        {
-          required: true,
-          message: '此项为必填项',
-        },
-      ],
-    },
-  },
-  {
-    title: '状态',
-    dataIndex: 'state',
-    filters: true,
-    onFilter: true,
-    valueType: 'select',
-    valueEnum: {
-      all: { text: '全部', status: 'Default' },
-      open: {
-        text: '未解决',
-        status: 'Error',
-      },
-      closed: {
-        text: '已解决',
-        status: 'Success',
-        disabled: true,
-      },
-      processing: {
-        text: '解决中',
-        status: 'Processing',
-      },
-    },
-  },
-  {
-    title: '标签',
-    dataIndex: 'labels',
-    search: false,
-    renderFormItem: (_, { defaultRender }) => defaultRender(_),
-    render: (_, record) => (
-      <Space>
-        {record.labels.map(({ name, color }) => (
-          <Tag color={color} key={name}>
-            {name}
-          </Tag>
-        ))}
-      </Space>
-    ),
-  },
-  {
-    title: '创建时间',
-    key: 'showTime',
-    dataIndex: 'created_at',
-    valueType: 'dateTime',
-    sorter: true,
-    hideInSearch: true,
-  },
-  {
-    title: '创建时间',
-    dataIndex: 'created_at',
-    valueType: 'dateRange',
-    hideInTable: true,
-    search: {
-      transform: (value) => ({
-        startTime: value[0],
-        endTime: value[1],
-      }),
-    },
-  },
-  {
-    title: '操作',
-    valueType: 'option',
-    render: (text, record, _, action) => [
-      <a
-        key="editable"
-        onClick={() => {
-          action?.startEditable?.(record.id);
-        }}
-      >
-        编辑
-      </a>,
-      <a href={record.url} target="_blank" rel="noopener noreferrer" key="view">
-        查看
-      </a>,
-      <TableDropdown
-        key="actionGroup"
-        onSelect={() => action?.reload()}
-        menus={[
-          { key: 'copy', name: '复制' },
-          { key: 'delete', name: '删除' },
-        ]}
-      />,
-    ],
-  },
-];
-
 export default function RecentLaunches() {
   const [data, setData] = useState();
   const [smallData, setSmallData] = useState();
@@ -412,11 +306,17 @@ export default function RecentLaunches() {
   const showModal = (record) => {
     setIsModalVisible(true);
     // console.log(record.mission_name);
+    let smallName;
+    if (record.mission_name.indexOf('(') > -1) {
+      smallName = record.mission_name.substring(0, record.mission_name.indexOf('('));
+    } else {
+      smallName = record.mission_name;
+    }
     const req = 'http://api.spacex.land/graphql/';
 
     const query = gql`
     {
-      launchesPastResult(find: {mission_name:"${record.mission_name}"}){
+      launchesPastResult(find: {mission_name:"${smallName}"}){
         data {
           links {
             mission_patch_small
